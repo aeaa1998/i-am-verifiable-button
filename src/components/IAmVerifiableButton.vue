6cc7478fd853b35dfd5d1729219e326bb7753d82
Augusto Alonso <augustoalonso98@gmail.com>
2022-10-24 14:41:57 -0600

    added log option

--- src/components/IAmVerifiableButton.vue
+++ src/components/IAmVerifiableButton.vue
@@ -14,8 +14,12 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  log: {
+    type: Boolean,
+    default: false,
+  },
 });
-
+const { log } = props;
 const emit = defineEmits(["verification:started", "verification:ended", "verification:succeded", "verification:failed", "update:isVerifying"]);
 
 const isVerifyingComputed = computed({
@@ -25,6 +29,7 @@ const isVerifyingComputed = computed({
 
 //Action of verification
 const verifyButton = async () => {
+  const startTime = new Date();
   emit("verification:started");
   isVerifyingComputed.value = true;
   const value = await useIamVerification(props.requisites ?? []);
@@ -35,6 +40,11 @@ const verifyButton = async () => {
     emit("verification:failed");
   }
   emit("verification:ended");
+  const endTime = new Date();
+  const timeDifference = (endTime - startTime) / 1000;
+  if (log) {
+    console.log("Verification duration was ", Math.round(timeDifference));
+  }
   return value;
 };
 </script>
